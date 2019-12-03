6ff7b7c355e376f0ea55b3d5bce50ead2790f1ab
hubciu24 <58463657+hubciu24@users.noreply.github.com>
2019-12-03 15:39:36 +0100

    Add new files

---
+++ ORLY.py
@@ -0,0 +1,43 @@
+import random
+
+def welcome():
+    print(
+        """
+             |-----------------------------------------------|
+             |               WITAJ W GRZE                    |
+             |              ORZEL VS RESZKA                  | 
+             |-----------------------------------------------|
+        """
+    )
+def computer():
+    result = random.randint(1, 2)
+    return result
+
+def player():
+    choice = input("Wybierz orła (O) lub Reszkę (R)")
+    if choice == "O":
+        choice = 1
+    elif choice == "R":
+        choice = 2
+    else:
+        print("Błąd!")
+    return choice
+
+#main
+
+welcome()
+play = player()
+comp = computer()
+
+if comp == 1:
+    print("ORZEŁ!")
+elif comp == 2:
+    print("RESZKA!")
+
+if play == comp:
+    print("WYGRAŁEŚ!")
+else:
+    print("Przegrałeś!")
+
+input("\nNaciśnij Enter aby wyjść!")
+
